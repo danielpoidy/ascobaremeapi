f125f4c00b0b5d56a8c36b838efa53721e578919
Mac <mac@MacBook-Pro-de-Mac.local>
2022-06-26 01:29:59 +0000

    mise en place basique pour deploiement

--- baremebackend/settings.py
+++ baremebackend/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'baremebackend.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASE = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl)}
+DATABASES = {'default': config('DATABASE_URL', default=default_dburl, cast=dburl)}
 
 
 # Password validation
