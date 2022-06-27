caea0f249dd27639f146b8e67ae8ad790b9c1b2d
Mac <mac@MacBook-Pro-de-Mac.local>
2022-06-27 13:48:41 +0000

    Modifications pour deploiement

--- baremebackend/settings.py
+++ baremebackend/settings.py
@@ -16,9 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',cast=bool)
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ascobareme.herokuapp.com/']
+# ALLOWED_HOSTS = ['127.0.0.1', 'ascobareme.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
