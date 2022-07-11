aaa68e1794167355f20265c04c69eff12be8031d
Mac <mac@MacBook-Pro-de-Mac.local>
2022-07-11 03:26:12 +0000

    cors headers2

--- baremebackend/settings.py
+++ baremebackend/settings.py
@@ -112,7 +112,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS: True
+# CORS_ALLOW_ALL_ORIGINS: True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "http://localhost:3000/",
+    "http://127.0.0.1:3000",
+]
 
 # CORS_ALLOW_METHODS = [
 #     "GET"
